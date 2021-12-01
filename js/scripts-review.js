43d6b8e7f9dd1dc81a3a4525dae5f1df08385cbe
Geoffrey Lisch <geoffrey.lisch@gmail.com>
2021-12-01 16:58:19 -0700

    review page creation

--- js/scripts-review.js
+++ js/scripts-review.js
@@ -2,26 +2,27 @@ const tab = document.querySelector("#overallBtn");
 tab.addEventListener("click", pillFocus);
 
 function pillFocus () {
-  document.getElementById("pills-product-tab").click();
+  document.getElementById("v-pills-product-tab").click();
 }
 
 const tab1 = document.querySelector("#experienceBtn");
 tab1.addEventListener("click", pillFocus1);
 
 function pillFocus1 () {
-  document.getElementById("pills-service-tab").click();
+  document.getElementById("v-pills-service-tab").click();
 }
 
 const tab2 = document.querySelector("#serviceBtn");
 tab2.addEventListener("click", pillFocus2);
 
 function pillFocus2 () {
-  document.getElementById("pills-cost-tab").click();
+  document.getElementById("v-pills-cost-tab").click();
 }
 
 const tab3 = document.querySelector("#costBtn");
 tab3.addEventListener("click", pillFocus3);
 
 function pillFocus3 () {
-  document.getElementById("pills-final-tab").click();
+  document.getElementById("v-pills-final-tab").click();
 }
+
