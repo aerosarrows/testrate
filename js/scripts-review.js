ab44cbd28194e6bfe0809d8f32958a6312deb5ee
Geoffrey Lisch <geoffrey.lisch@gmail.com>
2021-12-02 12:38:10 -0700

    last initial changes

--- js/scripts-review.js
+++ js/scripts-review.js
@@ -2,20 +2,6 @@ const tab = document.querySelector("#overallBtn");
 tab.addEventListener("click", pillFocus);
 
 function pillFocus () {
-  document.getElementById("v-pills-product-tab").click();
-}
-
-const tab1 = document.querySelector("#experienceBtn");
-tab1.addEventListener("click", pillFocus1);
-
-function pillFocus1 () {
-  document.getElementById("v-pills-service-tab").click();
-}
-
-const tab2 = document.querySelector("#serviceBtn");
-tab2.addEventListener("click", pillFocus2);
-
-function pillFocus2 () {
   document.getElementById("v-pills-cost-tab").click();
 }
 
@@ -23,6 +9,22 @@ const tab3 = document.querySelector("#costBtn");
 tab3.addEventListener("click", pillFocus3);
 
 function pillFocus3 () {
+  document.getElementById("v-pills-service-tab").click();
+}
+
+const tab2 = document.querySelector("#serviceBtn");
+tab2.addEventListener("click", pillFocus2);
+
+function pillFocus2 () {
+  document.getElementById("v-pills-product-tab").click();
+}
+
+const tab1 = document.querySelector("#qualityBtn");
+tab1.addEventListener("click", pillFocus1);
+
+function pillFocus1 () {
   document.getElementById("v-pills-final-tab").click();
 }
+
+
 
